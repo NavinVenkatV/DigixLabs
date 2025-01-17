694eb64dffce4b9deaff2ab0c5f6aa0bf81ef9b0
NavinVenkatV <vnavinvenkat@gmail.com>
2025-01-18 03:27:50 +0530

    init

---
+++ src/app/Component/Image.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Images() {
+  return (
+    <div>
+      <img src="/bg.jpg" alt="LOADING..." className='rounded-full py-10'/>
+    </div>
+  )
+}
+
+export default Images
