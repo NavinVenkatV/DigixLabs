08bdc6e744eaa40f83a6cae80b446db626d11412
nirmalraj1610 <nirmal16ags@gmail.com>
2025-01-23 12:26:56 +0530

    init

--- src/app/Component/PricingTag.tsx
+++ src/app/Component/PricingTag.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function PricingTag() {
+export default function PricingTag() {
     return (
         <div className='p-11 bg-pink-500 rounded-3xl mt-32 '>
-            <p className='text-center text-black text-3xl'>$200/Month</p>
+            <p className='text-center text-black text-3xl'>$0/Month</p>
+            <p className='items-center flex justify-center'>Early Bid*</p>
             <div className='mt-10'>
             <p>✓ 2 Technical Blog Posts</p>
             <p>✓ 2 Thought Leadership Articles</p>
@@ -17,4 +18,22 @@ function PricingTag() {
     )
 }
 
-export default PricingTag
+export  function PricingTag2() {
+    return (
+        <div className='p-11 bg-pink-500 rounded-3xl mt-32 '>
+            <p className='text-center text-black text-3xl'>$44/Month</p>
+            <p className='items-center flex justify-center'>20% off*</p>
+
+            <div className='mt-10'>
+            <p>✓ 4 Technical Blog Posts</p>
+            <p>✓ 4 Thought Leadership Articles</p>
+            <p>✓ Advanced Social Media Management</p>
+            <p>✓ Advanced Community Engagement</p>
+            </div>
+            <button className='w-full py-2 mt-10 bg-black text-white text-2xl rounded-3xl flex flex-col justify-center items-center hover:text-pink-300'>
+                Get Started
+            </button>
+        </div>
+    )
+}
+
--- src/app/read/page.tsx
+++ src/app/read/page.tsx
@@ -6,13 +6,19 @@ import Lenis from "@studio-freight/lenis/types";
 import Footer from "../Component/Footer";
 import Yt from "../Component/yt";
 
+import { Quattrocento } from 'next/font/google';
+
+const font = Quattrocento({
+  subsets : ['latin'],
+  weight : '400'
+})
 
 export default function Home() {
 
   
 
   return (
-    <div id="smooth" className="w-full h-full overflow-hidden m-0 p-0">
+    <div id="smooth" className={`w-full h-full  overflow-hidden m-0 p-0 ${font.className}`}>
       <Header />
       <div className="py-5">
         <Yt/>
