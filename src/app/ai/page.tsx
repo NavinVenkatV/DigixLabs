a9414fdcba5d6077b7ba958648247d7aabbee55e
NavinVenkatV <vnavinvenkat@gmail.com>
2025-01-23 15:12:24 +0530

    yt

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -15,7 +15,7 @@ export default function page() {
       <Header />
       <div className='text-center items-center flex justify-center w-full h-full'>
         <div className='w-screen h-screen flex flex-col justify-center items-center text-pink-500 lg:text-3xl'>
-          <div className='p-5'>Dont&apos; Worry, We'=&apos;ll launch this page before  GTA VI 🤠</div>
+          <div className='p-5'>Dont&apos; Worry, We&apos;ll launch this page before  GTA VI 🤠</div>
         <Image
         src={"https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGIxMTh5bDhrZmg5ZXAxaGlmYmN3b3ZsOW01b2Y2aWk4ZWh5NTNidSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/o0vwzuFwCGAFO/giphy.gif"}
         alt={"gif"}
